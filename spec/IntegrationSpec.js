511e155d8e5eb03107e6472b2a63000ac9050f14
Marc Roulias <marc@marcroulias.com>
2017-08-19 03:41:17 -0700

    got tests to pass

--- spec/IntegrationSpec.js
+++ spec/IntegrationSpec.js
@@ -1,12 +1,29 @@
-const AdapterIntegrationSpec = require('../node_modules/bass/spec/AdapterIntegrationSpec');
 
-describe('bass-redis', AdapterIntegrationSpec('bass-redis', {
+const redis = require('redis');
+const AdapterIntegration = require('../node_modules/bass/spec/AdapterIntegration');
+
+const client = redis.createClient({
+    host: 'localhost',
+    port: '6379'
+});
+
+client.on("ready", function(err) {
+
+    client.flushall(() => {
+
+        console.log('flushed');
+
+    });
+
+});
+
+describe('bass-redis', AdapterIntegration('bass-redis', {
     connections: {
         default: {
             adapter: 'bass-redis',
             host: 'localhost',
             port: '6379',
-            database: 'test'
+            database: 'bass-redis-test'
         }
     }
 }));
